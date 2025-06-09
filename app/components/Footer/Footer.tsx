23f7c519b3c910e2fd1ab34436e02441e58b3a08
N0L1CZE <stankopatrik875@gmail.com>
2025-06-09 16:40:59 +0200

    scaling of bottom footer logo

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
     <footer className={styles.footer}>
       <div className={styles.footerContainer}>
         <Link href="/" className={styles.footerLogoLink}>
-          <Image src="/logo-black.png" alt="Forhaus UH" width={181} height={130} priority />
+          <Image src="/logo-black.png" alt="Forhaus UH" width={181} height={102} priority />
         </Link>
         <div className={styles.footerCopy}>
           copyright © 2025 Forhaus-UH
